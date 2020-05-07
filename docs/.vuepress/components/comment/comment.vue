244748939eebaa380eb9c7c6b1dd2b53316d4755
sheng <sheng@live.cn>
2020-05-07 23:10:00 +0800

    ''

--- docs/.vuepress/components/comment/comment.vue
+++ docs/.vuepress/components/comment/comment.vue
@@ -3,6 +3,7 @@
     <div id="gitalk-container"></div>
   </div>
 </template>
+ <script src="/js/md5.min.js"></script>
 <script>
 export default {
   name: 'comment',
@@ -29,7 +30,7 @@ export default {
         // id 用于当前页面的唯一标识，一般来讲 pathname 足够了，
        
         // 但是如果你的 pathname 超过 50 个字符，GitHub 将不会成功创建 issue，此情况可以考虑给每个页面生成 hash 值的方法.
-        id: location.pathname,
+        id: md5(location.pathname),
         distractionFreeMode: false,
         language: 'zh-CN'
       };
